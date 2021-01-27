71e59fe5c334c4c631ddaa7d23db371d23256ee0
Heying Yu <heying.yu@vanderbilt.edu>
2021-01-27 12:15:00 -0600

    auth flow done

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//dev.js- don't commit
+module.exports = {
+    googleClientID: '129318342070-598ud46gnkuriuqp8lvin4mbi3tf4ngs.apps.googleusercontent.com',
+    googleClientSecret: 'LjcuATYs6z5U_fSb8sg8lJeG',
+    mongoURI: 'mongodb+srv://emaily:JeblttSRWH23SUQq@emaily.afh10.mongodb.net/Emaily?retryWrites=true&w=majority',
+    cookieKey: 'TczaFlw1234567',
+}
