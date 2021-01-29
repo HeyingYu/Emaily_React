2ef783e52d80059b161244ff38d2fa2d50b64cdf
Heying Yu <heying.yu@vanderbilt.edu>
2021-01-28 23:05:40 -0600

    test deployment

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
-//dev.js- don't commit
 module.exports = {
     googleClientID: '129318342070-598ud46gnkuriuqp8lvin4mbi3tf4ngs.apps.googleusercontent.com',
     googleClientSecret: 'LjcuATYs6z5U_fSb8sg8lJeG',
-    mongoURI: 'mongodb+srv://emaily:JeblttSRWH23SUQq@emaily.afh10.mongodb.net/Emaily?retryWrites=true&w=majority',
+    mongoURI: 'mongodb+srv://emaily:jhXqvYDAOlnHguEV@emaily.afh10.mongodb.net/Emaily?retryWrites=true&w=majority',
     cookieKey: 'TczaFlw1234567',
-}
+    stripePublishableKey: 'pk_test_51IEmVEGJwofkOpnHN3aNTUCScqRsSSg2ZpbM2NyyZONuIlkOM4xirNT9qWjy3dyy7TODAA2yFYdw7aakruDZjwiH00Tq7nZW28',
+    stripeSecretKey: 'sk_test_51IEmVEGJwofkOpnHgtjA9GGL4X0ZSbxaTZ5aeyuVk2JVhCjKuwNM1bfMSGgO3NvE8asdi4znN0LM2beOdW8XCXt600V3D5e9hE',
+  };
