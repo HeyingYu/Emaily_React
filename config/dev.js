2beee97408e7a8967a4be74e5ebc850980cda868
Heying Yu <heying.yu@vanderbilt.edu>
2021-01-29 18:25:31 -0600

    survey finished

--- config/dev.js
+++ config/dev.js
@@ -5,4 +5,6 @@ module.exports = {
     cookieKey: 'TczaFlw1234567',
     stripePublishableKey: 'pk_test_51IEmVEGJwofkOpnHN3aNTUCScqRsSSg2ZpbM2NyyZONuIlkOM4xirNT9qWjy3dyy7TODAA2yFYdw7aakruDZjwiH00Tq7nZW28',
     stripeSecretKey: 'sk_test_51IEmVEGJwofkOpnHgtjA9GGL4X0ZSbxaTZ5aeyuVk2JVhCjKuwNM1bfMSGgO3NvE8asdi4znN0LM2beOdW8XCXt600V3D5e9hE',
+    sendGridKey: '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',
+    redirectDomain:'http://localhost:3000/'
   };
